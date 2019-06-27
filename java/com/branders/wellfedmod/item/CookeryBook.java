88b22c47445a6acf0b9f223b18bea664899a8ac2
Anders Blomqvist <anders@minaemail.se>
2019-06-27 20:44:32 +0200

    Fixed server crash

--- java/com/branders/wellfedmod/item/CookeryBook.java
+++ java/com/branders/wellfedmod/item/CookeryBook.java
@@ -1,7 +1,7 @@
 package com.branders.wellfedmod.item;
 
-import com.branders.wellfedmod.gui.GuiCookeryBook;
-import com.branders.wellfedmod.gui.GuiCookeryBookPagesList;
+//import com.branders.wellfedmod.gui.GuiCookeryBook;
+//import com.branders.wellfedmod.gui.GuiCookeryBookPagesList;
 import com.branders.wellfedmod.lists.ItemList;
 
 import net.minecraft.client.Minecraft;
@@ -12,6 +12,8 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class CookeryBook extends Item
 {
@@ -20,20 +22,21 @@ public class CookeryBook extends Item
 	/**
 	 * 	Open Book GUI when right click with this item
 	 */
+	@OnlyIn(Dist.CLIENT)
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) 
 	{
 		Minecraft mc = Minecraft.getInstance();
-		
-		if(!worldIn.isRemote)
-			mc.addScheduledTask(() -> mc.displayGuiScreen(new GuiCookeryBook(GuiCookeryBookPagesList.getRecipeBookPages())));
+
+		if(worldIn.isRemote)
+			mc.addScheduledTask(() -> mc.displayGuiScreen(new com.branders.wellfedmod.gui.GuiCookeryBook(com.branders.wellfedmod.gui.GuiCookeryBookPagesList.getRecipeBookPages())));
 		
 		ItemStack book = new ItemStack(ItemList.cookery_book);
 		return new ActionResult<>(EnumActionResult.SUCCESS, book);
 	}
 	
 	@Override
-	public boolean hasEffect(ItemStack stack) 
+	public boolean hasEffect(ItemStack stack)
 	{
 		return false;
 	}
